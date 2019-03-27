3c6769eab02af788f92b1d298c5937e5e5424834
ricegood <yeonjaekoo@gmail.com>
2019-03-27 13:48:06 +0900

    accelerometer sample app

---
+++ Accelerometer/inc/accelerometer.h
@@ -0,0 +1,19 @@
+#ifndef __accelerometer_H__
+#define __accelerometer_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "accelerometer"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.accelerometer"
+#endif
+
+#endif /* __accelerometer_H__ */
---
+++ Accelerometer/src/accelerometer.c
@@ -0,0 +1,231 @@
+#include "accelerometer.h"
+#include <sensor.h>
+#include <app.h>
+#include "dlog.h"
+#include <Elementary.h>
+
+
+/* callback function */
+void example_sensor_callback(sensor_h sensor, sensor_event_s *event, void *user_data) {
+
+    /*
+     If a callback is used to listen for different sensor types,
+     it can check the sensor type
+     */
+    sensor_type_e type;
+    sensor_get_type(sensor, &type);
+
+    if (type == SENSOR_ACCELEROMETER) {
+        dlog_print(DLOG_DEBUG, "sensor callback", "SENSOR_ACCELEROMETER on");
+        unsigned long long timestamp = event->timestamp;
+
+        // Print timestamp
+        char tmp[30];
+        sprintf(tmp,"time : %lld",timestamp);
+        dlog_print(DLOG_DEBUG, "sensor callback", tmp);
+
+        // ?
+        int accuracy = event->accuracy;
+
+        // Print accelerometer data
+        float x = event->values[0];
+        float y = event->values[1];
+        float z = event->values[2];
+        sprintf(tmp,"%f %f %f",x,y,z);
+        dlog_print(DLOG_DEBUG, "sensor callback", tmp);
+    }
+}
+
+static void initial_sensor(){
+
+	// Check weather the SENSOR is supported (SENSOR_ACCELEROMETER)
+    bool supported = false;
+    sensor_is_supported(SENSOR_ACCELEROMETER, &supported);
+    if (!supported) {
+        dlog_print(DLOG_DEBUG, "sensor", "sensor is not supported" );
+        return;
+        /* Accelerometer is not supported on the current device */
+    }
+    dlog_print(DLOG_DEBUG, "sensor","sensor is supported" );
+
+
+    // Get the sensor handler using sensor ID
+    sensor_h sensor;
+    sensor_get_default_sensor(SENSOR_ACCELEROMETER, &sensor);
+
+    dlog_print(DLOG_DEBUG, "sensor","sensor handler get" );
+
+
+    // Create listener handler using sensor handler
+    sensor_listener_h listener;
+    sensor_create_listener(sensor, &listener);
+
+
+    // Register callback function (callback interval = 100ms)
+    sensor_listener_set_event_cb(listener, 100, example_sensor_callback, NULL);
+
+    // Start Listener
+    sensor_listener_start(listener);
+
+    //sensor_listener_stop(listener);
+    //sensor_destroy_listener(listener);
+}
+
+
+typedef struct appdata {
+	Evas_Object *win;
+	Evas_Object *conform;
+	Evas_Object *label;
+} appdata_s;
+
+static void
+win_delete_request_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	ui_app_exit();
+}
+
+static void
+win_back_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	appdata_s *ad = data;
+	/* Let window go to hide state. */
+	elm_win_lower(ad->win);
+}
+
+static void
+create_base_gui(appdata_s *ad)
+{
+	/* Window */
+	ad->win = elm_win_util_standard_add(PACKAGE, PACKAGE);
+	elm_win_autodel_set(ad->win, EINA_TRUE);
+
+	if (elm_win_wm_rotation_supported_get(ad->win)) {
+		int rots[4] = { 0, 90, 180, 270 };
+		elm_win_wm_rotation_available_rotations_set(ad->win, (const int *)(&rots), 4);
+	}
+
+	evas_object_smart_callback_add(ad->win, "delete,request", win_delete_request_cb, NULL);
+	eext_object_event_callback_add(ad->win, EEXT_CALLBACK_BACK, win_back_cb, ad);
+
+	/* Conformant */
+	ad->conform = elm_conformant_add(ad->win);
+	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
+	elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
+	evas_object_size_hint_weight_set(ad->conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	elm_win_resize_object_add(ad->win, ad->conform);
+	evas_object_show(ad->conform);
+
+	/* Label */
+	ad->label = elm_label_add(ad->conform);
+	elm_object_text_set(ad->label, "<align=center>ACCELEROMETER</align>");
+	evas_object_size_hint_weight_set(ad->label, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	elm_object_content_set(ad->conform, ad->label);
+
+	/* Show window after base gui is set up */
+	evas_object_show(ad->win);
+}
+
+static bool
+app_create(void *data)
+{
+	/* Hook to take necessary actions before main event loop starts
+		Initialize UI resources and application's data
+		If this function returns true, the main loop of application starts
+		If this function returns false, the application is terminated */
+	appdata_s *ad = data;
+	create_base_gui(ad);
+	initial_sensor(); // initialize sensor
+
+	return true;
+}
+
+static void
+app_control(app_control_h app_control, void *data)
+{
+	/* Handle the launch request. */
+}
+
+static void
+app_pause(void *data)
+{
+	/* Take necessary actions when application becomes invisible. */
+}
+
+static void
+app_resume(void *data)
+{
+	/* Take necessary actions when application becomes visible. */
+}
+
+static void
+app_terminate(void *data)
+{
+	/* Release all resources. */
+}
+
+static void
+ui_app_lang_changed(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_LANGUAGE_CHANGED*/
+	char *locale = NULL;
+	system_settings_get_value_string(SYSTEM_SETTINGS_KEY_LOCALE_LANGUAGE, &locale);
+	elm_language_set(locale);
+	free(locale);
+	return;
+}
+
+static void
+ui_app_orient_changed(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_DEVICE_ORIENTATION_CHANGED*/
+	return;
+}
+
+static void
+ui_app_region_changed(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_REGION_FORMAT_CHANGED*/
+}
+
+static void
+ui_app_low_battery(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_LOW_BATTERY*/
+}
+
+static void
+ui_app_low_memory(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_LOW_MEMORY*/
+}
+
+int
+main(int argc, char *argv[])
+{
+
+	appdata_s ad = {0,};
+	int ret = 0;
+
+	ui_app_lifecycle_callback_s event_callback = {0,};
+	app_event_handler_h handlers[5] = {NULL, };
+
+	event_callback.create = app_create;
+	event_callback.terminate = app_terminate;
+	event_callback.pause = app_pause;
+	event_callback.resume = app_resume;
+	event_callback.app_control = app_control;
+
+	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY], APP_EVENT_LOW_BATTERY, ui_app_low_battery, &ad);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY], APP_EVENT_LOW_MEMORY, ui_app_low_memory, &ad);
+	ui_app_add_event_handler(&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED], APP_EVENT_DEVICE_ORIENTATION_CHANGED, ui_app_orient_changed, &ad);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED], APP_EVENT_LANGUAGE_CHANGED, ui_app_lang_changed, &ad);
+	ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED], APP_EVENT_REGION_FORMAT_CHANGED, ui_app_region_changed, &ad);
+	ui_app_remove_event_handler(handlers[APP_EVENT_LOW_MEMORY]);
+
+	ret = ui_app_main(argc, argv, &event_callback, &ad);
+	if (ret != APP_ERROR_NONE) {
+		dlog_print(DLOG_ERROR, LOG_TAG, "app_main() is failed. err = %d", ret);
+	}
+
+	return ret;
+}
